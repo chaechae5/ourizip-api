a7c05f8956b3839c805607e16766c4af2cff9019
eunchae oh <eunchae1083@gmail.com>
2026-03-24 10:59:45 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/kotlin/com/ourizip/ourizip_api/OurizipApiApplication.kt
@@ -0,0 +1,11 @@
+package com.ourizip.ourizip_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OurizipApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<OurizipApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/ourizip/ourizip_api/OurizipApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ourizip.ourizip_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OurizipApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
